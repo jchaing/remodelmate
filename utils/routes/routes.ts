13820ef0f252a2fc3197a8a1f8b0f511f8d68a36
John Chaing <john@spinningplates.org>
2023-02-13 08:33:28 -0800

    added route maps

---
+++ utils/routes/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTE_MAP = {
+  app: {
+    entry: '/',
+    collections: '/collections',
+    contractors: '/contractors',
+    transformations: '/transformations',
+    aboutUs: '/about-us',
+    faq: '/faq',
+    blogs: '/blogs',
+    press: '/press',
+    tos: '/terms-of-service',
+    privacy: '/privacy-policy',
+    help: '/help',
+    newCityLaunch: '/new-city-launch'
+  }
+}
