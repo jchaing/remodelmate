dc059f939ef556fca4232709ba1f394b664451c7
John Chaing <john@spinningplates.org>
2023-03-20 16:31:35 -0700

    refactored collections dropdown menu from nav

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,4 +1,11 @@
-import { Fragment, FunctionComponent, ReactNode, useState } from 'react'
+/* eslint-disable @next/next/no-img-element */
+import {
+  Fragment,
+  FunctionComponent,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import {
   XIcon,
@@ -8,171 +15,9 @@ import {
 } from '@heroicons/react/outline'
 import { Footer } from '@components/footer'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { ROUTE_MAP } from '@utils/routes'
-
-const navigation = {
-  categories: [
-    {
-      id: 'collections',
-      name: 'Collections',
-      featured: [
-        {
-          name: 'Collections 1',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt:
-            'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Collections 2',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt:
-            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-        {
-          name: 'Collections 3',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt:
-            'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Collections 4',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt:
-            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-        {
-          name: 'Collections 5',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-          imageAlt:
-            'Models sitting back to back, wearing Basic Tee in black and bone.',
-        },
-        {
-          name: 'Collections 6',
-          href: '#',
-          imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
-          imageAlt:
-            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        },
-      ],
-      sections: [
-        {
-          id: 'collections_page',
-          name: 'Collections Page',
-          href: ROUTE_MAP.app.collections,
-          items: [
-            { name: 'Collection 1', href: '#' },
-            { name: 'Collection 2', href: '#' },
-            { name: 'Collection 3', href: '#' },
-            { name: 'Collection 4', href: '#' },
-            { name: 'Collection 5', href: '#' },
-            { name: 'Collection 6', href: '#' },
-            // { name: 'Jackets', href: '#' },
-            // { name: 'Activewear', href: '#' },
-            // { name: 'Browse All', href: '#' },
-          ],
-        },
-        // {
-        //   id: 'accessories',
-        //   name: 'Accessories',
-        //   items: [
-        //     { name: 'Watches', href: '#' },
-        //     { name: 'Wallets', href: '#' },
-        //     { name: 'Bags', href: '#' },
-        //     { name: 'Sunglasses', href: '#' },
-        //     { name: 'Hats', href: '#' },
-        //     { name: 'Belts', href: '#' },
-        //   ],
-        // },
-        // {
-        //   id: 'brands',
-        //   name: 'Brands',
-        //   items: [
-        //     { name: 'Full Nelson', href: '#' },
-        //     { name: 'My Way', href: '#' },
-        //     { name: 'Re-Arranged', href: '#' },
-        //     { name: 'Counterfeit', href: '#' },
-        //     { name: 'Significant Other', href: '#' },
-        //   ],
-        // },
-      ],
-    },
-    // {
-    //   id: 'men',
-    //   name: 'Men',
-    //   featured: [
-    //     {
-    //       name: 'New Arrivals',
-    //       href: '#',
-    //       imageSrc:
-    //         'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-    //       imageAlt:
-    //         'Drawstring top with elastic loop closure and textured interior padding.',
-    //     },
-    //     {
-    //       name: 'Artwork Tees',
-    //       href: '#',
-    //       imageSrc:
-    //         'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
-    //       imageAlt:
-    //         'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-    //     },
-    //   ],
-    //   sections: [
-    //     {
-    //       id: 'clothing',
-    //       name: 'Clothing',
-    //       items: [
-    //         { name: 'Tops', href: '#' },
-    //         { name: 'Pants', href: '#' },
-    //         { name: 'Sweaters', href: '#' },
-    //         { name: 'T-Shirts', href: '#' },
-    //         { name: 'Jackets', href: '#' },
-    //         { name: 'Activewear', href: '#' },
-    //         { name: 'Browse All', href: '#' },
-    //       ],
-    //     },
-    //     {
-    //       id: 'accessories',
-    //       name: 'Accessories',
-    //       items: [
-    //         { name: 'Watches', href: '#' },
-    //         { name: 'Wallets', href: '#' },
-    //         { name: 'Bags', href: '#' },
-    //         { name: 'Sunglasses', href: '#' },
-    //         { name: 'Hats', href: '#' },
-    //         { name: 'Belts', href: '#' },
-    //       ],
-    //     },
-    //     {
-    //       id: 'brands',
-    //       name: 'Brands',
-    //       items: [
-    //         { name: 'Re-Arranged', href: '#' },
-    //         { name: 'Counterfeit', href: '#' },
-    //         { name: 'Full Nelson', href: '#' },
-    //         { name: 'My Way', href: '#' },
-    //       ],
-    //     },
-    //   ],
-    // },
-  ],
-  pages: [
-    { name: 'Contractors', href: ROUTE_MAP.app.contractors },
-    { name: 'About us', href: ROUTE_MAP.app.aboutUs },
-  ],
-}
+import { getStoryblokApi } from '@storyblok/react'
+import Image from 'next/image'
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
@@ -182,19 +27,46 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
   children,
 }) => {
   const [open, setOpen] = useState(false)
+  const [collections, setCollections] = useState<any[]>([])
 
-  const onClick = () => {
-    const event = new KeyboardEvent('keydown', {
-      key: 'Escape',
-      code: 'Escape',
-      keyCode: 27,
-      which: 27,
-      bubbles: true,
-    })
-    document.dispatchEvent(event)
+  const navigation = {
+    categories: [
+      {
+        id: 'collections',
+        name: 'Collections',
+        featured: [
+          ...collections,
+        ],
+      },
+    ],
+    pages: [
+      { name: 'Contractors', href: ROUTE_MAP.app.contractors },
+      { name: 'About us', href: ROUTE_MAP.app.aboutUs },
+    ],
   }
 
-  const router = useRouter()
+  useEffect(() => {
+    const storyblokApi = getStoryblokApi()
+
+    const getCollections = async () => {
+      const sbParams: any = {
+        version: process.env.NEXT_PUBLIC_STORYBLOK_VERSION,
+        starts_with: 'collections/',
+        is_startpage: false,
+      }
+
+      const { data } = await storyblokApi.get('cdn/stories', sbParams)
+
+      setCollections((prev) =>
+        data.stories.slice(0, 6).map((collection: any) => {
+          collection.content.slug = collection.slug
+          return collection
+        })
+      )
+    }
+
+    getCollections()
+  }, [])
 
   return (
     <div className="bg-white">
@@ -262,23 +134,34 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
                         key={category.name}
                         className="space-y-10 px-4 pt-10 pb-8"
                       >
-                        <div className="grid grid-cols-2 gap-x-4">
+                        <div className="flex items-center justify-center">
+                          <Link
+                            href={ROUTE_MAP.app.collections}
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="text-md">
+                              Go to Collections Page
+                            </span>
+                          </Link>
+                        </div>
+                        <div className="grid grid-cols-2 gap-8">
                           {category.featured.map((item) => (
-                            <Link
+                            <div
                               key={item.name}
                               className="group relative text-sm"
-                              href={item.href}
                               onClick={() => setOpen(false)}
                             >
                               <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                <img
-                                  src={item.imageSrc}
-                                  alt={item.imageAlt}
+                                <Image
+                                  src={item.content.body[0].images[0].filename}
+                                  alt={item.content.body[0].images[0].alt}
                                   className="object-cover object-center"
+                                  width={128}
+                                  height={128}
                                 />
                               </div>
-                              <a
-                                href={item.href}
+                              <Link
+                                href={item.full_slug}
                                 className="mt-6 block font-medium text-gray-900"
                               >
                                 <span
@@ -286,45 +169,13 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
                                   aria-hidden="true"
                                 />
                                 {item.name}
-                              </a>
-                              <p aria-hidden="true" className="mt-1">
+                              </Link>
+                              {/* <p aria-hidden="true" className="mt-1">
                                 Shop now
-                              </p>
-                            </Link>
+                              </p> */}
+                            </div>
                           ))}
                         </div>
-                        {category.sections.map((section) => (
-                          <div key={section.name}>
-                            <p
-                              id={`${category.id}-${section.id}-heading-mobile`}
-                              className="font-medium text-gray-900"
-                            >
-                              <Link
-                                href={section.href}
-                                onClick={() => setOpen(false)}
-                              >
-                                {section.name}
-                              </Link>
-                            </p>
-                            <ul
-                              role="list"
-                              aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                              className="mt-6 flex flex-col space-y-6"
-                            >
-                              {section.items.map((item) => (
-                                <li key={item.name} className="flow-root">
-                                  <Link
-                                    href={item.href}
-                                    className="-m-2 block p-2 text-gray-500"
-                                    onClick={() => setOpen(false)}
-                                  >
-                                    {item.name}
-                                  </Link>
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                        ))}
                       </Tab.Panel>
                     ))}
                   </Tab.Panels>
@@ -461,24 +312,41 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
 
                               <div className="relative">
                                 <div className="mx-auto max-w-7xl px-8">
+                                  <div className="flex items-center justify-center pt-8">
+                                    <Link
+                                      href={ROUTE_MAP.app.collections}
+                                      onClick={() => close()}
+                                    >
+                                      <span className="text-lg">
+                                        Go to Collections Page
+                                      </span>
+                                    </Link>
+                                  </div>
                                   <div className="grid gap-y-10 gap-x-8 py-16">
-                                    <div className="col-start-6 grid grid-cols-6 gap-x-16">
+                                    <div className="grid grid-cols-6 gap-x-16">
                                       {category.featured.map((item) => (
                                         <Link
-                                          href={item.href}
+                                          href={item.full_slug}
                                           key={item.name}
                                           className="group relative text-base sm:text-sm"
                                           onClick={() => close()}
                                         >
                                           <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                            <img
-                                              src={item.imageSrc}
-                                              alt={item.imageAlt}
+                                            <Image
+                                              src={
+                                                item.content.body[0].images[0]
+                                                  .filename
+                                              }
+                                              alt={
+                                                item.content.body[0].images[0]
+                                                  .alt
+                                              }
                                               className="object-cover object-center"
+                                              width={150}
+                                              height={150}
                                             />
                                           </div>
-                                          <a
-                                            href={item.href}
+                                          <div
                                             className="mt-6 block font-medium text-gray-900"
                                           >
                                             <span
@@ -486,51 +354,8 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
                                               aria-hidden="true"
                                             />
                                             {item.name}
-                                          </a>
-                                          <p
-                                            aria-hidden="true"
-                                            className="mt-1"
-                                          >
-                                            Shop now
-                                          </p>
+                                          </div>
                                         </Link>
-                                      ))}
-                                    </div>
-                                    <div className="row-start-1 grid grid-cols-1 gap-y-10 gap-x-8 text-sm">
-                                      {category.sections.map((section) => (
-                                        <div key={section.name}>
-                                          <p
-                                            id={`${section.name}-heading`}
-                                            className="font-medium text-gray-900"
-                                          >
-                                            <Link
-                                              href={section.href}
-                                              onClick={() => close()}
-                                            >
-                                              {section.name}
-                                            </Link>
-                                          </p>
-                                          <ul
-                                            role="list"
-                                            aria-labelledby={`${section.name}-heading`}
-                                            className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                          >
-                                            {section.items.map((item) => (
-                                              <li
-                                                key={item.name}
-                                                className="flex"
-                                              >
-                                                <Link
-                                                  href={item.href}
-                                                  className="hover:text-gray-800"
-                                                  onClick={() => close()}
-                                                >
-                                                  {item.name}
-                                                </Link>
-                                              </li>
-                                            ))}
-                                          </ul>
-                                        </div>
                                       ))}
                                     </div>
                                   </div>
@@ -588,15 +413,15 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
                 </div> */}
 
                 {/* Search */}
-                <div className="flex lg:ml-6">
+                {/* <div className="flex lg:ml-6">
                   <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
                     <span className="sr-only">Search</span>
                     <SearchIcon className="h-6 w-6" aria-hidden="true" />
                   </a>
-                </div>
+                </div> */}
 
                 {/* Cart */}
-                <div className="ml-4 flow-root lg:ml-6">
+                {/* <div className="ml-4 flow-root lg:ml-6">
                   <a href="#" className="group -m-2 flex items-center p-2">
                     <ShoppingBagIcon
                       className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
@@ -607,7 +432,7 @@ export const Navigation: FunctionComponent<NavigationProps> = ({
                     </span>
                     <span className="sr-only">items in cart, view bag</span>
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
