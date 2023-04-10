c7975fb1f3d7ac2c0e290779b6fd6c5cf1847513
John Chaing <john@spinningplates.org>
2023-04-10 14:38:27 -0700

    added daisyui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,16 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('daisyui'),
   ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'light',
+  },
 }
