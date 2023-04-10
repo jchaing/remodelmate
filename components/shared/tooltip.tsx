1f6f1f38158186a050dafaec55939ee76eb9616f
John Chaing <john@spinningplates.org>
2023-04-10 14:41:10 -0700

    added tooltip for phone title

---
+++ components/shared/tooltip.tsx
@@ -0,0 +1,44 @@
+export const Tooltip = ({ message }: {message: string}) => {
+  const checkAndCloseDropDown = (e: any) => {
+    const targetEl = e.currentTarget
+    if (targetEl && targetEl.matches(':focus')) {
+      setTimeout(function () {
+        targetEl.blur()
+      }, 0)
+    }
+  }
+
+  return (
+    <div className="dropdown dropdown-bottom">
+      <label
+        tabIndex={0}
+        className="btn btn-circle btn-ghost btn-xs text-info"
+        onMouseDown={checkAndCloseDropDown}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="h-4 w-4 stroke-sky-500"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+      </label>
+      <div
+        tabIndex={0}
+        className="card compact dropdown-content bg-base-100 rounded-box -right-28 w-64 shadow"
+      >
+        <div className="card-body">
+          <ul className="pl-4 text-sm font-medium text-gray-500">
+            <li>{message}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -1,4 +1,5 @@
 import { SignInNav } from '@components/navigation'
+import { Tooltip } from '@components/shared'
 import Link from 'next/link'
 
 const SignIn = () => {
@@ -17,15 +18,18 @@ const SignIn = () => {
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white px-4 py-8 sm:shadow sm:rounded-lg sm:px-10">
+          <div className="bg-white px-4 py-8 sm:rounded-lg sm:px-10 sm:shadow">
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Phone
-                </label>
+                <div className="flex">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Phone
+                  </label>
+                  <Tooltip message="We'll send you an SMS code to your mobile phone to complete your sign-in, which will then log you directly into your account dashboard." />
+                </div>
                 <div className="mt-2">
                   <input
                     id="email"
@@ -38,55 +42,10 @@ const SignIn = () => {
                 </div>
               </div>
 
-              {/* <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div> */}
-
-              {/* <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-sky-600 hover:text-sky-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div> */}
-
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+                  className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 focus-visible:outline"
                 >
                   Sign in
                 </button>
@@ -99,9 +58,7 @@ const SignIn = () => {
                   <div className="w-full border-t border-gray-300" />
                 </div>
                 <div className="relative flex justify-center text-sm">
-                  <span className="bg-white px-2 text-gray-500">
-                    Or
-                  </span>
+                  <span className="bg-white px-2 text-gray-500">Or</span>
                 </div>
               </div>
 
@@ -111,7 +68,9 @@ const SignIn = () => {
                     href="#"
                     className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
                   >
-                    <span className="">Create Account</span>
+                    <span className="text-sm font-semibold">
+                      Create Account
+                    </span>
                   </Link>
                 </div>
 
