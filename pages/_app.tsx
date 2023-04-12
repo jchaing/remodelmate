2f40ed10487d3fc1e244f0e536e0a36ed1c5cf91
John Chaing <john@spinningplates.org>
2023-04-12 13:19:29 -0700

    added react-query plugin and configured react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ReactElement, ReactNode } from 'react'
+import { ReactElement, ReactNode, useState } from 'react'
 import Head from 'next/head'
 import { NextPage } from 'next'
 import { storyblokInit, apiPlugin } from '@storyblok/react'
@@ -36,7 +36,14 @@ import {
   PrivacyPolicyHeader,
 } from '@components/storyblok/privacy-policy'
 import { FaqContractors, FaqHomeowners } from '@components/storyblok/faqs'
-import { AboutUsCta, AboutUsFunding, AboutUsHero, AboutUsStory, AboutUsTeam } from '@components/storyblok/about-us'
+import {
+  AboutUsCta,
+  AboutUsFunding,
+  AboutUsHero,
+  AboutUsStory,
+  AboutUsTeam,
+} from '@components/storyblok/about-us'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
 const components = {
   page: Page,
@@ -88,6 +95,13 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+      })
+  )
+
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
@@ -101,7 +115,12 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 
         <title>{`remodelmate | Effortless Bathroom Renovations`}</title>
       </Head>
-      {getLayout(<Component {...pageProps} />)}
+
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          {getLayout(<Component {...pageProps} />)}
+        </Hydrate>
+      </QueryClientProvider>
     </>
   )
 }
