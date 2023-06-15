b3a49bd7a9fa439c214610d4a0f8f38541aa4b38
John Chaing <john@spinningplates.org>
2023-06-15 07:18:01 -0700

    added google tag manager scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,45 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Start of Google Tag Manager (noscript) */}
+        <Script
+          id="gtm-script-remodelmate"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KMCV6WV');
+                `,
+          }}
+        />
+        {/* End of Google Tag Manager (noscript) */}
+
+        {/* Start Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="0tKLUvVGi_4fGRcqMiuqDxticWV8cmnqV8vQcOkOI-E"
+        />
+        {/* End Google Search Console Verification */}
+      </Head>
       <body>
+        {/* Start of Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMCV6WV"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                `,
+          }}
+        />
+        {/* End of Google Tag Manager (noscript) */}
+
         <Main />
         <NextScript />
       </body>
